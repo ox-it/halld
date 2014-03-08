fd14bb4c34bc4ef1d103c15181708400b06020e5
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-03-08 09:27:24 +0000

    Added packages to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,14 @@ from distutils.core import setup
 setup(
     name='oxpoints',
     version='0.1',
-    packages=['halld', 'halld.util'],
+    packages=[
+        'halld',
+        'halld.management'
+        'halld.pubsub'
+        'halld.registry'
+        'halld.test'
+        'halld.test_site'
+        'halld.util'
+    ],
 )
+
