31b066d3ebfd865fd279c6a33f108f570c9915b0
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-04-20 13:32:37 +0100

    Split out type metadata and resource lists in URLs.

--- halld/views/index.py
+++ halld/views/index.py
@@ -8,17 +8,24 @@ __all__ = ['IndexView']
 class IndexView(HALLDView):
     def get(self, request):
         self.context['_links'] = {
-            'type:{}'.format(resource_type.name): {
-                'href': reverse('halld:resource-type', args=[resource_type.name])}
-            for resource_type in get_resource_types().values()
-        }
-        self.context['_links'].update({
-            'findResourceType': {'href': '/{resourceType}',
+            'findResourceType': {'href': '/type/{resourceType}',
+                           'templated': True},
+            'findResourceList': {'href': '/{resourceType}',
                            'templated': True},
             'findResource': {'href': '/{resourceType}/{identifier}',
                            'templated': True},
             'findSource': {'href': '/{resourceType}/{identifier}/source/{source}',
                            'templated': True},
             'findByIdentifier': {'href': reverse('halld:by-identifier')}
+        }
+        self.context['_links'].update({
+            'items:{}'.format(resource_type.name): {
+                'href': reverse('halld:resource-list', args=[resource_type.name])}
+            for resource_type in get_resource_types().values()
+        })
+        self.context['_links'].update({
+            'type:{}'.format(resource_type.name): {
+                'href': reverse('halld:resource-type-detail', args=[resource_type.name])}
+            for resource_type in get_resource_types().values()
         })
         return self.render()
