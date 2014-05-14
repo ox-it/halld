0ff25693c2efb536354d37747a021a4353e6d989
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-05-14 13:33:10 +0100

    Add simple test to make sure resource list returns

--- halld/test/__init__.py
+++ halld/test/__init__.py
@@ -1,5 +1,6 @@
 from .extant import *
 from .identifiers import *
 from .inference import *
+from .resource import *
 from .resource_creation import *
 from .sources import *
---
+++ halld/test/resource.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.test import RequestFactory, TestCase
+
+from .. import views
+
+class ResourceTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_superuser(username='superuser',
+                                                  email='superuser@example.com',
+                                                  password='secret')
+        self.resource_list_view = views.ResourceListView.as_view()
+        self.resource_detail_view = views.ResourceDetailView.as_view()
+        self.source_view = views.SourceDetailView.as_view()
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def testGetResourceList(self):
+        request = self.factory.get('/snake')
+        self.resource_list_view(request, 'snake')
