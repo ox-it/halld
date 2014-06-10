f25e7ff8d6cc61d36c10e10e002ab59cfb02d666
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-06-10 15:32:35 +0100

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 from distutils.core import setup
 
+# Idea borrowed from http://cburgmer.posterous.com/pip-requirementstxt-and-setuppy
+install_requires, dependency_links = [], []
+for line in open('requirements.txt'):
+    line = line.strip()
+    if line.startswith('-e'):
+        dependency_links.append(line[2:].strip())
+    elif line:
+        install_requires.append(line)
+
+try:
+    import enum
+except ImportError:
+    install_requires.append('enum34')
+
 setup(
     name='halld',
     version='0.1',
@@ -12,5 +26,7 @@ setup(
         'halld.test_site',
         'halld.util',
     ],
+    install_requires=install_requires,
+    dependency_links=dependency_links,
 )
 
