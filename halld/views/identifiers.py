99e5ddfcd01d215b9e1aa244466206c786bf4390
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-09-28 23:04:54 +0100

    Identifier view can now return all with identifier scheme; JSON Schema docs

--- halld/views/identifiers.py
+++ halld/views/identifiers.py
@@ -14,22 +14,36 @@ class ByIdentifierView(JSONRequestMixin):
         'properties': {
             'scheme': {
                 'type': 'string',
+                'description': 'The identifier scheme for all identifiers you wish to return results.',
             },
             'values': {
                 'type': 'array',
                 'items': {'type': 'string'},
                 'uniqueItems': True,
+                'description': 'A list of identifier values for the resources you wish to retrieve.',
+            },
+            'allInScheme': {
+                'type': 'boolean',
+                'enum': [True],
+                'description': 'If true, all resources with an identifier in the specified scheme will be retrieved.',
             },
             'includeData': {
                 'type': 'boolean',
+                'description': 'If true, the response will return the full descriptions of the matched resources. Default false.',
             },
             'includeSources': {
                 'type': 'array',
                 'items': {'type': 'string'},
                 'uniqueItems': True,
+                'description': 'A list of source type names you want returning with the results. If missing or empty, none will be returned.',
             },
         },
-        'required': ['scheme', 'values'],
+        'required': ['scheme'],
+        'oneOf': [{
+            'required': ['values'],
+        }, {
+            'required': ['allInScheme'],
+        }],
     }
     def post(self, request):
         query = self.get_request_json()
@@ -37,15 +51,19 @@ class ByIdentifierView(JSONRequestMixin):
             jsonschema.validate(query, self.schema)
         except jsonschema.ValidationError as e:
             raise exceptions.SchemaValidationError(e)
-        identifiers = Identifier.objects.filter(scheme=query['scheme'],
-                                                value__in=query['values']).select_related('resource')
+
+        identifiers = Identifier.objects.filter(scheme=query['scheme'])
+        if 'values' in query:
+            identifiers = identifiers.filter(value__in=query['values'])
+        identifiers = identifiers.select_related('resource')
+
         if query.get('includeSources'):
             identifiers = identifiers.select_related('resource__source_set')
         seen_values = set()
         results = {}
         for identifier in identifiers:
             resource = identifier.resource
-            result = {'type': resource.type_id, 'identifier': resource.identifier}
+            result = {'type': resource.type_id, 'identifier': resource.identifier, 'resourceHref': resource.pk}
             if query.get('includeData'):
                 data = resource.filter_data(request.user, resource.data)
                 result['data'] = resource.get_hal(request.user, data)
@@ -56,6 +74,7 @@ class ByIdentifierView(JSONRequestMixin):
                     result['sources'][source.type_id] = source.get_hal(request.user)
             results[identifier.value] = result
             seen_values.add(identifier.value)
-        for value in set(query['values']) - seen_values:
-            results[value] = None
+        if 'values' in query:
+            for value in set(query['values']) - seen_values:
+                results[value] = None
         return HttpResponse(json.dumps(results, indent=2), content_type='application/hal+json')
