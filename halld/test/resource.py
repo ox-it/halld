95ae2464f02f3b627996371200b261c47f39a7a2
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-11-05 13:38:03 +0000

    Tidy up ResourceListTestCase

--- halld/test/resource.py
+++ halld/test/resource.py
@@ -1,22 +1,7 @@
-from django.contrib.auth.models import User, AnonymousUser
-from django.test import RequestFactory, TestCase
+from .base import TestCase
 
-from .. import views
-
-class ResourceTestCase(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.user = User.objects.create_superuser(username='superuser',
-                                                  email='superuser@example.com',
-                                                  password='secret')
-        self.resource_list_view = views.ResourceListView.as_view()
-        self.resource_detail_view = views.ResourceDetailView.as_view()
-        self.source_view = views.SourceDetailView.as_view()
-
-    def tearDown(self):
-        User.objects.all().delete()
-
+class ResourceListTestCase(TestCase):
     def testGetResourceList(self):
         request = self.factory.get('/snake')
-        request.user = AnonymousUser()
+        request.user = self.anonymous_user
         self.resource_list_view(request, 'snake')
