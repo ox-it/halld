4496b98305fac67e6e27245d045be9ec89e0cf3f
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-11-08 20:33:59 +0000

    Force all changesets to be authored by authenticated users.

--- halld/views/changeset.py
+++ halld/views/changeset.py
@@ -10,6 +10,8 @@ from .mixins import JSONRequestMixin
 
 class ChangesetView(ObjectCacheView, JSONView, JSONRequestMixin):
     def get_new_changeset(self, data):
+        if not self.request.user.is_authenticated():
+            raise exceptions.Forbidden(self.request.user)
         return Changeset(base_href=self.request.build_absolute_uri(),
                          author=self.request.user,
                          data=data)
@@ -17,7 +19,6 @@ class ChangesetView(ObjectCacheView, JSONView, JSONRequestMixin):
 class ChangesetListView(ChangesetView):
     def post(self, request):
         data = self.get_request_json('application/json')
-
         changeset = self.get_new_changeset(data)
         changeset.perform(multiple=True,
                           object_cache=self.object_cache)
