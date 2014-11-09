5b6aa1f4ae07e71e14ed81d662af82859e8bf0f9
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-11-09 23:10:47 +0000

    Test trying to PUT a source where the resource doesn't exist

--- halld/test/changeset.py
+++ halld/test/changeset.py
@@ -4,6 +4,8 @@ import json
 from .. import exceptions
 from .base import TestCase
 
+from ..test_site.registry import SnakeResourceTypeDefinition
+
 class ChangesetTestCase(TestCase):
     def testAddingUnsupportedSource(self):
         _, identifier = self.create_resource()
@@ -28,3 +30,25 @@ class ChangesetTestCase(TestCase):
         self.assertEqual(error_json['error'], 'multiple-errors')
         self.assertEqual(error_json['_embedded']['error'][0]['error'],
                          'incompatible-source-type')
+
+    def testEditingSourceWithMissingResource(self):
+        identifier = SnakeResourceTypeDefinition().generate_identifier()
+        href = 'http://testserver/snake/' + identifier
+        changeset = {
+            'updates': [{
+                'method': 'PUT',
+                'resourceHref': href,
+                'sourceType': 'conjecture',
+                'data': {'wears': 'clothing'},
+            }],
+        }
+        request = self.factory.post('/changeset',
+                                    data=json.dumps(changeset),
+                                    content_type='application/json')
+        request.user = self.superuser
+
+        with self.assertRaises(exceptions.SourceDataWithoutResource) as cm:
+            self.changeset_list_view(request)
+        error_json = cm.exception.as_hal()
+        self.assertEqual(error_json['_links']['missingResources'],
+                         [{'href': href}])
