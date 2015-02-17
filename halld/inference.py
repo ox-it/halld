2355fe09cafb268c7cc1d0b73b55f77252c31a1a
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-02-17 21:03:47 +0000

    Add a Lookup inference for finding a link by identifier

--- halld/inference.py
+++ halld/inference.py
@@ -61,6 +61,33 @@ class FirstOf(FromPointers):
                                             ', '.join(map(repr, self.pointers)),
                                             ', update=True' if self.update else '')
 
+class Lookup(FromPointers):
+    def __init__(self, target, scheme, *pointers):
+        self.target = jsonpointer.JsonPointer(target)
+        self.scheme = scheme
+        self.pointers = list(map(jsonpointer.JsonPointer, pointers))
+
+    def __call__(self, resource, data, **kwargs):
+        for pointer in self.pointers:
+            try:
+                value = data.resolve(pointer)
+                break
+            except jsonpointer.JsonPointerException:
+                continue
+        else:
+            return
+
+        try:
+            identifier = models.Identifier.objects.select_related('resource').get(scheme=self.scheme,
+                                                                                  value=value)
+        except models.Identifier.DoesNotExist:
+            logger.warning("Can't lookup identifier '%s' in scheme '%s' for resource '%s' and pointer '%s'",
+                           value, self.scheme,
+                           resource.href, pointer.path)
+        else:
+            other = identifier.resource
+            data.set(self.target, other.href)
+
 class Set(FromPointers):
     """
     Inference that unions the data from a set of source pointers.
